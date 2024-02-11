1759bdae47145961b88c963754924cc22b2f1bdf
Jérémy Morel <jeremy.meme@hotmail.fr>
2024-02-11 18:41:21 +0100

    Form contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
